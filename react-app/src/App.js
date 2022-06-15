818921e3615479a9bbb15f6f6f06799ae317ae05
user <yenje7@naver.com>
2022-06-15 11:38:40 +0900

    Route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,45 +1,51 @@
+import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
-import { padding } from "@mui/system";
+import styled from "styled-components";
+import { Link, Routes, Route, useParams } from "react-router-dom";
 
 function Header(props) {
-  const myStyle = {
-    borderBottom: "1px solid skyblue",
-    padding: "10px",
-  };
   return (
-    <header style={myStyle}>
+    <header className={props.className}>
       <h1>
-        <a
-          href="/"
-          onClick={(event) => {
-            console.log("event", event);
-            event.preventDefault();
+        <Link
+          to="/"
+          onClick={(evt) => {
             props.onSelect();
           }}
         >
           WWW
-        </a>
+        </Link>
       </h1>
     </header>
   );
 }
-
+const HeaderStyled = styled(Header)`
+  border-bottom: 1px solid gray;
+  color: red;
+`;
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
 function Nav(props) {
   const liTags = props.data.map((e) => {
     return (
       <li key={e.id}>
-        <a
-          href={"/read/" + e.id}
-          onClick={(event) => {
-            event.preventDefault();
+        <Link
+          to={"/read/" + e.id}
+          onClick={(evt) => {
             props.onSelect(e.id);
           }}
         >
           {e.title}
-        </a>
+        </Link>
       </li>
     );
   });
@@ -49,27 +55,42 @@ function Nav(props) {
     </nav>
   );
 }
-
-function Article(props) {
+function Create(props) {
   return (
     <article>
-      <h2>{props.title}</h2>
-      {props.body}
+      <h2>Create</h2>
+      <form
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          const title = evt.target.title.value;
+          const body = evt.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title"></input>
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create"></input>
+        </p>
+      </form>
     </article>
   );
 }
 function App() {
   const [mode, setMode] = useState("WELCOME");
   const [id, setId] = useState(null);
-  console.log(mode, id);
-  const topics = [
-    { id: 1, title: "html", body: "html is..." },
-    { id: 2, title: "css", body: "css is..." },
-  ];
+  const [nextId, setNextId] = useState(3);
+  const [topics, setTopics] = useState([
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+  ]);
   let content = null;
-
   if (mode === "WELCOME") {
-    content = <Article title="Welcome" body="Hello, WEB!" />;
+    content = <Article title="Welcome" body="Hello, WEB!"></Article>;
   } else if (mode === "READ") {
     const topic = topics.filter((e) => {
       if (e.id === id) {
@@ -78,37 +99,100 @@ function App() {
         return false;
       }
     })[0];
-    content = <Article title={topic.title} body={topic.body} />;
+    content = <Article title={topic.title} body={topic.body}></Article>;
+  } else if (mode === "CREATE") {
   }
-
   return (
     <div>
-      <Header
-        onSelect={() => {
-          setMode("WELCOME");
-        }}
-      />
-      <Nav
-        data={topics}
-        onSelect={(id) => {
-          setMode("READ");
-          setId(id);
-        }}
-      />
-      {content}
-      <ButtonGroup variant="contained" aria-label="outlined button group">
+      <HeaderStyled onSelect={headerHandler()}></HeaderStyled>
+      <Nav data={topics} onSelect={navHandler()}></Nav>
+      <Routes>
+        <Route
+          path="/"
+          element={<Article title="Welcome" body="Hello, WEB!"></Article>}
+        ></Route>
+        <Route
+          path="/create"
+          element={<Create onCreate={onCreateHandler()}></Create>}
+        ></Route>
+        <Route
+          path="/read/:topic_id"
+          element={<Read topics={topics}></Read>}
+        ></Route>
+      </Routes>
+      <ButtonGroup>
         <Button
+          component={Link}
+          to="/create"
           variant="outlined"
-          onClick={() => {
-            alert("create!");
-          }}
+          onClick={createHandler()}
         >
           Create
         </Button>
         <Button variant="outlined">Update</Button>
       </ButtonGroup>
-      <Button variant="outlined">Delete</Button>
+      <Button variant="outlined" onClick={deleteHandler()}>
+        Delete
+      </Button>
     </div>
   );
+  function Read(props) {
+    const params = useParams();
+    const id = Number(params.topic_id);
+    const topic = props.topics.filter((e) => {
+      if (e.id === id) {
+        return true;
+      } else {
+        return false;
+      }
+    })[0];
+    return <Article title={topic.title} body={topic.body}></Article>;
+  }
+
+  function onCreateHandler() {
+    return (title, body) => {
+      const newTopic = { id: nextId, title, body };
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setId(nextId);
+      setMode("READ");
+      setNextId(nextId + 1);
+    };
+  }
+
+  function navHandler() {
+    return (id) => {
+      setMode("READ");
+      setId(id);
+    };
+  }
+
+  function deleteHandler() {
+    return () => {
+      const newTopics = topics.filter((e) => {
+        if (e.id === id) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+      setMode("WELCOME");
+      setTopics(newTopics);
+    };
+  }
+
+  function createHandler() {
+    return () => {
+      setMode("CREATE");
+    };
+  }
+
+  function headerHandler() {
+    return () => {
+      setMode("WELCOME");
+    };
+  }
 }
+
 export default App;
