022d14c350f21b6fa35a0a3d8f897d8c08add503
user <yenje7@naver.com>
2022-06-20 10:21:13 +0900

    컴포넌트 생성

--- 0620/crud/src/App.js
+++ 0620/crud/src/App.js
@@ -1,5 +1,28 @@
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ol></ol>
+    </nav>
+  );
+}
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+    </div>
+  );
 }
 
 export default App;
