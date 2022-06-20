a22f266563189811ea136fdc05e207674aa8a5e7
user <yenje7@naver.com>
2022-06-20 13:34:52 +0900

    create, update, delete 구현

--- 0620/crud/src/App.js
+++ 0620/crud/src/App.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, Routes, Route, useParams } from "react-router-dom";
-
+import { Link, Routes, Route, useParams, useNavigate } from "react-router-dom";
 function Header() {
   return (
     <header>
@@ -10,7 +9,6 @@ function Header() {
     </header>
   );
 }
-
 function Nav(props) {
   return (
     <nav>
@@ -18,7 +16,7 @@ function Nav(props) {
         {props.data.map((e) => {
           return (
             <li key={e.id}>
-              <Link to={`read/${e.id}`}>{e.title}</Link>
+              <Link to={`/read/${e.id}`}>{e.title}</Link>
             </li>
           );
         })}
@@ -38,7 +36,7 @@ function Read(props) {
   };
   useEffect(() => {
     refreshTopic();
-  }, []);
+  }, [id]);
   return (
     <article>
       <h2>{topic.title}</h2>
@@ -46,7 +44,109 @@ function Read(props) {
     </article>
   );
 }
+function Create(props) {
+  const submitHandler = async (evt) => {
+    evt.preventDefault();
+    const title = evt.target.title.value;
+    const body = evt.target.body.value;
+    props.onCreate(title, body);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <h2>Create</h2>
+      <p>
+        <input type="text" name="title" placeholder="title"></input>
+      </p>
+      <p>
+        <textarea name="body" placeholder="body"></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  );
+}
+function Control(props) {
+  const param = useParams();
+  const id = Number(param.id);
+  let contextUI = null;
+  if (id) {
+    contextUI = (
+      <>
+        <Link to={"/update/" + id}>Update</Link>
+        <button
+          onClick={() => {
+            props.onDelete(id);
+          }}
+        >
+          delete
+        </button>
+      </>
+    );
+  }
+  return (
+    <ul>
+      <li>
+        <Link to="/create">Create</Link>
+      </li>
+      {contextUI}
+    </ul>
+  );
+}
+function Update(props) {
+  const param = useParams();
+  const id = Number(param.id);
+  const [title, setTitle] = useState(null);
+  const [body, setBody] = useState(null);
+  const refreshTopic = async () => {
+    const resp = await fetch("http://localhost:3333/topics/" + id);
+    const result = await resp.json();
+    setTitle(result.title);
+    setBody(result.body);
+  };
+  useEffect(() => {
+    refreshTopic();
+  }, [id]);
+
+  const navigate = useNavigate();
+  const submitHandler = async (evt) => {
+    evt.preventDefault();
+    const title = evt.target.title.value;
+    const body = evt.target.body.value;
+    props.onUpdate(id, title, body);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <h2>Update</h2>
+      <p>
+        <input
+          type="text"
+          name="title"
+          placeholder="title"
+          value={title}
+          onChange={(evt) => {
+            setTitle(evt.target.value);
+          }}
+        ></input>
+      </p>
+      <p>
+        <textarea
+          name="body"
+          placeholder="body"
+          value={body}
+          onChange={(evt) => {
+            setBody(evt.target.value);
+          }}
+        ></textarea>
+      </p>
+      <p>
+        <input type="submit" value="update" />
+      </p>
+    </form>
+  );
+}
 function App() {
+  const navigate = useNavigate();
   const [topics, setTopics] = useState([]);
   const refreshTopics = async () => {
     const resp = await fetch("http://localhost:3333/topics");
@@ -56,7 +156,38 @@ function App() {
   useEffect(() => {
     refreshTopics();
   }, []);
-  console.log(topics);
+  const createHandler = async (title, body) => {
+    const resp = await fetch("http://localhost:3333/topics", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, body }),
+    });
+    const result = await resp.json();
+    navigate("/read/" + result.id);
+    refreshTopics();
+  };
+  const updateHandler = async (id, title, body) => {
+    const resp = await fetch("http://localhost:3333/topics/" + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, body }),
+    });
+    const result = await resp.json();
+    navigate("/read/" + result.id);
+    refreshTopics();
+  };
+  const deleteHandler = async (id) => {
+    const resp = await fetch("http://localhost:3333/topics/" + id, {
+      method: "DELETE",
+    });
+    const result = await resp.json();
+    navigate("/");
+    refreshTopics();
+  };
   return (
     <div>
       <Header></Header>
@@ -71,6 +202,25 @@ function App() {
           }
         ></Route>
         <Route path="/read/:id" element={<Read></Read>}></Route>
+        <Route
+          path="/create"
+          element={<Create onCreate={createHandler}></Create>}
+        ></Route>
+        <Route
+          path="/update/:id"
+          element={<Update onUpdate={updateHandler}></Update>}
+        ></Route>
+      </Routes>
+      <Routes>
+        {["/", "/read/:id", "/create"].map((e) => {
+          return (
+            <Route
+              key={e}
+              path={e}
+              element={<Control onDelete={deleteHandler}></Control>}
+            ></Route>
+          );
+        })}
       </Routes>
     </div>
   );
